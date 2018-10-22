1c6ff124e45971eb7b7d858fa8f0330f9f09e4c9
Cristhian Fallas <35886616+Chris-Fallas@users.noreply.github.com>
2018-10-21 19:19:24 -0600

    libs

--- CanchasWs/build/generated-sources/ap-source-output/Model/Cancha_.java
+++ CanchasWs/build/generated-sources/ap-source-output/Model/Cancha_.java
@@ -8,11 +8,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-10-21T18:13:06")
-=======
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-10-21T11:54:56")
->>>>>>> mario/ws/jasper
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-10-21T19:16:48")
 @StaticMetamodel(Cancha.class)
 public class Cancha_ { 
 
